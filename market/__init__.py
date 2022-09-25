5bbb55d2963cf67fbd32330e09f9dcff42932d95
Sarosh Farhan <saroshfarhan6495@gmail.com>
2022-09-25 13:03:32 +0530

    added logout functionality.

--- market/__init__.py
+++ market/__init__.py
@@ -10,6 +10,8 @@ app.config['SECRET_KEY'] = 'f100789c2e9644e4b283ced7'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
+login_manager.login_view = "login_page"
+login_manager.login_message_category = "info"
 
 from market import routes
 
--- market/routes.py
+++ market/routes.py
@@ -4,7 +4,7 @@ from flask import render_template, redirect, url_for, flash
 from market.models import Item, User
 from market.forms import RegisterForm, LoginForm
 from market import db #it is located in __init__ file
-from flask_login import login_user
+from flask_login import login_user, logout_user, login_required
 
 @app.route('/')
 @app.route('/home')
@@ -13,6 +13,7 @@ def home_page():
 
 
 @app.route('/market')
+@login_required
 def market_page():
     items = Item.query.all()
     return render_template('market.html', items = items)
@@ -27,6 +28,8 @@ def register_page():
 
         db.session.add(user_to_create)
         db.session.commit()
+        login_user(user_to_create)
+        flash(f'Account created successfully! You are now logged in as: {user_to_create.username}', category='success')
         return redirect(url_for('market_page'))
     if form.errors != {}: #if there are no errors from the validations, form.errors is a dictionary so checking if it is empty
         for err_msg in form.errors.values():
@@ -47,3 +50,10 @@ def login_page():
         else:
             flash(f'Username and password do not match! Please try again', category='danger')
     return render_template('login.html', form=form)
+
+
+@app.route('/logout')
+def logout_page():
+    logout_user()
+    flash("You have been logged out!", category='info')
+    return redirect(url_for('home_page'))
